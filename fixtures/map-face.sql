b8f417a9086bc2c88643915fc919479817cf7c52
Daven Quinn <dev@davenquinn.com>
2017-09-17 00:03:01 -0700

    Added a bunch of map design things

--- fixtures/map-face.sql
+++ fixtures/map-face.sql
@@ -3,19 +3,16 @@ DROP MATERIALIZED VIEW IF EXISTS mapping.map_face CASCADE;
 CREATE MATERIALIZED VIEW mapping.map_face AS
 WITH face AS (
   SELECT
-    (ST_Dump(ST_Polygonize(e.geom))).geom geometry
-  FROM map_topology.edge_contact ec
-  JOIN map_topology.contact c ON ec.contact_id = c.id
-  JOIN map_topology.edge e ON ec.edge_id = e.edge_id
-  WHERE c.type LIKE '%contact%'
+    (ST_Dump(ST_Polygonize(e.geometry))).geom geometry
+  FROM map_topology.topology_edges e
+  WHERE e.topology = 'bedrock'
 ),
 polygon AS (
   SELECT
     p.type,
     p.geometry
   FROM map_digitizer.polygon p
-  LEFT JOIN mapping.unit unit ON p.type = unit.id
-  ORDER BY p.id
+  JOIN mapping.unit unit ON p.type = unit.id
 )
 SELECT
   row_number() OVER () face_id,
@@ -23,9 +20,9 @@ SELECT
   unit.id AS unit_id,
   coalesce(unit.color, '#000000') color
 FROM face
-  LEFT JOIN polygon p
-    ON p.geometry @ face.geometry
-LEFT JOIN mapping.unit unit ON p.type = unit.id
+JOIN polygon p
+    ON ST_Contains(face.geometry, p.geometry)
+JOIN mapping.unit unit ON p.type = unit.id
 WHERE face.geometry IS NOT NULL
 ORDER BY ST_Area(face.geometry);
 
