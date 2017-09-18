a4da0e3007096522df2c9fb3a742f2b4c77b9057
Daven Quinn <dev@davenquinn.com>
2017-09-17 22:36:18 -0700

    Updated map viewing data

--- fixtures/map-face.sql
+++ fixtures/map-face.sql
@@ -13,6 +13,7 @@ polygon AS (
     p.geometry
   FROM map_digitizer.polygon p
   JOIN mapping.unit unit ON p.type = unit.id
+  ORDER BY p.id
 )
 SELECT
   row_number() OVER () face_id,
@@ -24,6 +25,7 @@ JOIN polygon p
     ON ST_Contains(face.geometry, p.geometry)
 JOIN mapping.unit unit ON p.type = unit.id
 WHERE face.geometry IS NOT NULL
+  AND unit.id NOT IN ('basement','highlands')
 ORDER BY ST_Area(face.geometry);
 
 CREATE INDEX map_face_gix ON mapping.map_face USING GIST (geometry);
