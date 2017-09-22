3743f0fff342cc5442e5cd92e85cca42eef4274b
Daven Quinn <dev@davenquinn.com>
2017-09-22 00:17:40 -0700

    Updated views for map topology

--- fixtures/02-map-contact.sql
+++ fixtures/02-map-contact.sql
@@ -6,7 +6,7 @@ SELECT DISTINCT ON (f.face_id)
   m.unit_id
 FROM map_topology.face_data f
 LEFT JOIN mapping.map_face m
-  ON ST_Contains(m.geometry, ST_Centroid(f.geometry))
+  ON ST_Contains(m.geometry, ST_PointOnSurface(f.geometry))
 ), edge_unit AS (
 SELECT
   array_agg(unit_id::text) units,
@@ -15,29 +15,22 @@ FROM map_topology.edge_face e
 JOIN face_unit f
   ON e.face_id = f.face_id
   GROUP BY edge_id
-), contact_data AS (
+)
 SELECT
   e.edge_id id,
+  eu.units,
   c.id AS contact_id,
   c.map_width,
   t.color,
   t.id AS type,
   e.geom geometry
-FROM map_topology.edge_contact ec
+FROM edge_unit eu
+JOIN map_topology.edge_contact ec ON ec.edge_id = eu.edge_id
 JOIN map_topology.contact c ON ec.contact_id = c.id
 JOIN map_topology.edge_data e ON ec.edge_id = e.edge_id
 JOIN map_digitizer.linework_type t ON c.type = t.id
 WHERE t.topology = 'bedrock'
-)
-SELECT DISTINCT ON (c.id)
-  c.*,
-  units
-FROM edge_unit e
-JOIN contact_data c
-  ON e.edge_id = c.id
--- ONLY join where there is a boundary between two different,
--- defined units
-WHERE NOT coalesce(units[1] = ALL(units), true);
+  AND NOT units[1] = ALL(units);
 
 CREATE INDEX mapping_contact_gix ON mapping.contact USING GIST (geometry);
 
