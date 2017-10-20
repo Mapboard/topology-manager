84b7534b7d33afe72c75fb9a4b5d62ba77748814
Daven Quinn <dev@davenquinn.com>
2017-10-20 04:36:45 -0700

    Updated

--- display/fixtures/01-map-face.sql
+++ display/fixtures/01-map-face.sql
@@ -22,22 +22,33 @@ pf AS (
 SELECT
   p.id,
   p.type,
-  face.geometry
+  face.geometry,
+  p.geometry poly
 FROM face
 LEFT JOIN polygon p
     ON ST_Contains(face.geometry, p.geometry)
    AND p.topology = face.topology
 WHERE face.geometry IS NOT NULL
-)
+),
+-- Assign face that has the greatest area of polygons
+-- assigned to it within the feature
+with_greatest_area AS (
 SELECT DISTINCT ON (geometry)
- row_number() OVER () AS id,
- type unit_id,
- geometry,
- t.topology,
- t.color
+  geometry,
+  type
 FROM pf
-JOIN map_digitizer.polygon_type t ON type = t.id
-ORDER BY geometry, type;
+GROUP BY geometry, type
+ORDER BY geometry, ST_Area(ST_Union(poly)) DESC
+)
+SELECT
+  row_number() OVER () id,
+  type AS unit_id,
+  f.geometry,
+  t.topology,
+  t.color
+FROM with_greatest_area f
+JOIN map_digitizer.polygon_type t
+  ON type = t.id;
 
 CREATE INDEX map_face_gix ON mapping.map_face USING GIST (geometry);
 
