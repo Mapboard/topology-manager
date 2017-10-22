27d12ba709d62b171c7a0fa38437e316a9131321
Daven Quinn <dev@davenquinn.com>
2017-10-22 13:29:09 -0700

    Updated map fixture to be based on tables

--- display/fixtures/01-map-face.sql
+++ display/fixtures/01-map-face.sql
@@ -1,6 +1,18 @@
 -- Map face
-DROP MATERIALIZED VIEW IF EXISTS mapping.map_face CASCADE;
-CREATE MATERIALIZED VIEW mapping.map_face AS
+CREATE TABLE mapping.map_face (
+  id SERIAL PRIMARY KEY,
+  unit_id text REFERENCES mapping.unit (id),
+  topology text REFERENCES map_topology.sub_topology (id)
+);
+
+SELECT topology.AddTopoGeometryColumn('map_topology',
+  'mapping', 'map_face', 'geometry', 'MULTIPOLYGON');
+
+TRUNCATE TABLE mapping.map_face;
+SELECT pg_catalog.setval(pg_get_serial_sequence('mapping.map_face', 'id'),
+  MAX(id))
+  FROM mapping.map_face;
+
 WITH face AS (
   SELECT
     topology,
@@ -40,15 +52,11 @@ FROM pf
 GROUP BY geometry, type
 ORDER BY geometry, ST_Area(ST_Union(poly)) DESC
 )
+INSERT INTO mapping.map_face (unit_id,geometry,topology)
 SELECT
-  row_number() OVER () id,
   type AS unit_id,
-  f.geometry,
-  t.topology,
-  t.color
+  map_topology.addMapFace(geometry, 1),
+  t.topology
 FROM with_greatest_area f
 JOIN map_digitizer.polygon_type t
   ON type = t.id;
-
-CREATE INDEX map_face_gix ON mapping.map_face USING GIST (geometry);
-
