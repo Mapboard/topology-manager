516d942bd702c797259f8b3683c79f960d9e3c5c
Daven Quinn <dev@davenquinn.com>
2017-11-22 02:52:22 -0800

    Fixed IDs

--- topology/procedures/update-contact.sql
+++ topology/procedures/update-contact.sql
@@ -1,6 +1,6 @@
 INSERT INTO map_topology.contact (id, geometry, hash, topology)
 SELECT
-  id,
+  l.id,
   topology.toTopoGeom(geometry, 'map_topology',
     (SELECT layer_id FROM topology.layer
       WHERE schema_name='map_topology'
@@ -8,8 +8,8 @@ SELECT
   md5(ST_AsBinary(geometry))::uuid,
   t.topology
 FROM map_digitizer.linework l
-JOIN mapping.linework_type t
+JOIN map_digitizer.linework_type t
   ON l.type = t.id
-WHERE id = :id
-  AND t.topology != null
+WHERE l.id = :id
+  AND t.topology IS NOT null
 RETURNING (geometry IS null);
