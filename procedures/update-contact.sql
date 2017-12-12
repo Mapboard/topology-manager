96dff6d264b7f4fc19bafe7f94b8b5655bfccda6
Daven Quinn <dev@davenquinn.com>
2017-12-12 14:17:35 -0800

    Create new contact updating functions

--- procedures/update-contact.sql
+++ procedures/update-contact.sql
@@ -1,7 +1,6 @@
-INSERT INTO map_digitizer.linework
 SELECT
-  map_topology.update_linework_topo(linework)
-FROM map_digitizer.linework
+  map_topology.update_linework_topo(l)
+FROM map_digitizer.linework l
 WHERE geometry_hash IS null
 LIMIT 1;
 
