3c925ea30e84a2779ac2e2df261f9f56fc24c879
Daven Quinn <dev@davenquinn.com>
2017-12-27 02:34:10 -0500

    Switched to pl/sql function

--- fixtures/08-notify.sql
+++ fixtures/08-notify.sql
@@ -5,8 +5,11 @@ to incrementally build views on each update.
 */
 CREATE OR REPLACE FUNCTION map_topology.linework_notify()
 RETURNS TRIGGER AS $$
-SELECT pg_notify('events',(TG_OP || 'on linework'));
-$$ LANGUAGE sql;
+BEGIN
+  PERFORM pg_notify('events',(TG_OP || 'on linework'));
+  RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
 
 -- Trigger to notify if linework has been changed
 DROP TRIGGER IF EXISTS map_topology_linework_notify_trigger
