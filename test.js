275a980d70caadd900e15b23ec47eb6709aeec2c
Daven Quinn <dev@davenquinn.com>
2020-12-28 11:11:56 -0500

    Added a basic passing test

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
+require("coffeescript/register");
 const test = require("ava");
+const { db, sql } = require("./src/util");
 
 test("should pass", (t) => {
   t.pass();
@@ -7,4 +9,13 @@ test("should pass", (t) => {
 test("should pass async", async (t) => {
   const bar = Promise.resolve("res");
   t.is(await bar, "res");
+});
+
+test("insert data", async (t) => {
+  const s1 = sql("test-fixtures/basic-insert");
+  const res = await db.query(s1);
+  t.is(res.length, 1);
+  console.log(res[0]);
+  t.is(res[0]["type"], "bedrock");
+  t.pass();
 });
