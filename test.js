6c835835172279ef163cfc2a40b0386be398fa43
Daven Quinn <dev@davenquinn.com>
2020-12-28 21:55:01 -0500

    More reproducible test

--- test.js
+++ test.js
@@ -2,18 +2,28 @@ require("coffeescript/register");
 const test = require("ava");
 const { db, sql } = require("./src/util");
 
-test("should pass", (t) => {
+test("basic insert", async (t) => {
+  const s1 = sql("test-fixtures/basic-insert");
+  const res = await db.query(s1);
+  t.is(res.length, 1);
+  console.log(res[0]);
+  t.is(res[0]["type"], "bedrock");
   t.pass();
 });
 
-test("should pass async", async (t) => {
-  const bar = Promise.resolve("res");
-  t.is(await bar, "res");
-});
-
-test("insert data", async (t) => {
-  const s1 = sql("test-fixtures/basic-insert");
-  const res = await db.query(s1);
+test("insert using stored procedure", async (t) => {
+  const s1 = sql("./packages/mapboard-server/sql/new-line");
+  const res = await db.query(s1, {
+    schema: "map_digitizer",
+    snap_width: 0,
+    snap_types: [],
+    type: "bedrock",
+    pixel_width: null,
+    map_width: null,
+    certainty: null,
+    zoom_level: null,
+    geometry: "LINESTRING(0 0,1 0)",
+  });
   t.is(res.length, 1);
   console.log(res[0]);
   t.is(res[0]["type"], "bedrock");
