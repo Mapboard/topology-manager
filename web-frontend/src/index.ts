40f4490dcd9fde498f89e1624fa9781b1abb6aab
Daven Quinn <dev@davenquinn.com>
2021-03-21 02:44:29 -0500

    Made into a basic react app

--- web-frontend/src/index.ts
+++ web-frontend/src/index.ts
@@ -1,88 +1,11 @@
-import "babel-polyfill";
-import { createStyle, createGeologySource, getStyle } from "./map-style";
-import io from "socket.io-client";
-import { get } from "axios";
-import { debounce } from "underscore";
-import mapboxgl from "mapbox-gl";
-import mbxUtils from "mapbox-gl-utils";
-import "mapbox-gl/dist/mapbox-gl.css";
+import h from "@macrostrat/hyper";
+import { render } from "react-dom";
+import { Map } from "./map";
 import "./main.styl";
 
-mapboxgl.accessToken = process.env.MAPBOX_TOKEN;
+function App() {
+  return h(Map);
+}
 
-let ix = 0;
-let oldID = "geology";
-const reloadGeologySource = function (map) {
-  const layerIDs = [
-    "unit",
-    "bedrock-contact",
-    "surface",
-    "surficial-contact",
-    "watercourse",
-    "line",
-  ];
-
-  ix += 1;
-  const newID = `geology-${ix}`;
-  map.addSource(newID, createGeologySource());
-  map.U.setLayerSource(layerIDs, newID);
-  map.removeSource(oldID);
-  return (oldID = newID);
-};
-
-(async function () {
-  const { data: polygonTypes } = await get(
-    "http://localhost:3006/polygon/types"
-  );
-  //const style = createStyle(polygonTypes);
-
-  const baseStyle = await getStyle({ access_token: mapboxgl.accessToken });
-  const style = createStyle(baseStyle, polygonTypes);
-
-  const map = new mapboxgl.Map({
-    container: "map",
-    style,
-    //style: "mapbox://styles/jczaplewski/cklb8aopu2cnv18mpxwfn7c9n",
-    hash: true,
-    center: [16.1987, -24.2254],
-    zoom: 10,
-  });
-
-  //map.setStyle("mapbox://styles/jczaplewski/cklb8aopu2cnv18mpxwfn7c9n");
-
-  map.on("load", function () {
-    map.addSource("mapbox-dem", {
-      type: "raster-dem",
-      url: "mapbox://mapbox.mapbox-terrain-dem-v1",
-      tileSize: 512,
-      maxzoom: 14,
-    });
-    // add the DEM source as a terrain layer with exaggerated height
-    map.setTerrain({ source: "mapbox-dem", exaggeration: 1.5 });
-
-    // add a sky layer that will show when the map is highly pitched
-    map.addLayer({
-      id: "sky",
-      type: "sky",
-      paint: {
-        "sky-type": "atmosphere",
-        "sky-atmosphere-sun": [0.0, 0.0],
-        "sky-atmosphere-sun-intensity": 15,
-      },
-    });
-  });
-
-  mbxUtils.init(map, mapboxgl);
-
-  const _ = function () {
-    console.log("Reloading map");
-    return reloadGeologySource(map);
-  };
-  const reloadMap = debounce(_, 500);
-
-  const socket = io();
-  return socket.on("topology", function (message) {
-    console.log(message);
-    return reloadMap();
-  });
-})();
+const el = document.getElementById("app");
+render(h(App), el);
