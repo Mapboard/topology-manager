00c962697cc72431692067e21f201820b95e20fb
Daven Quinn <dev@davenquinn.com>
2021-05-06 15:02:03 -0500

    Added missing dependencies

--- extensions/live-tiles/src/clear-cache.ts
+++ extensions/live-tiles/src/clear-cache.ts
@@ -1,13 +1,3 @@
-/*
- * decaffeinate suggestions:
- * DS101: Remove unnecessary use of Array.from
- * DS102: Remove unnecessary code created because of implicit returns
- * DS202: Simplify dynamic range loops
- * DS205: Consider reworking code to avoid use of IIFEs
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
-
 const { db, prepare } = require("../../../src/util");
 
 const command = "clear-tile-cache";
