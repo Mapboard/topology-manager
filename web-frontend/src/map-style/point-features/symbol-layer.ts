734cf632ee1746c8df9e9843e394fa2836202831
Daven Quinn <dev@davenquinn.com>
2021-10-19 23:59:10 -0500

    Moved symbol definition information around

--- web-frontend/src/map-style/point-features/symbol-layer.ts
+++ web-frontend/src/map-style/point-features/symbol-layer.ts
@@ -2,6 +2,7 @@
  * Create a map style for StraboSpot features. This is lifted directly from the
  * StraboSpot codebase, https://github.com/StraboSpot/StraboSpot-Mobile/blob/master/src/modules/maps/useMapSymbology.js
  */
+import { getIconImageExt } from "./icon-image";
 
 const pointLayers = () => {
   const isShowSpotLabelsOn = false;
@@ -111,135 +112,6 @@ const pointLayers = () => {
       ["literal", [0.75, 0]],
     ];
   };
-
-  const getIconImage = () => {
-    /** Get the image for a symbol using case-based logic. Taken as-is from StraboSpot codebase. Currently somewhat broken. */
-    return [
-      "case",
-      ["has", "orientation"],
-      // Variable bindings
-      [
-        "let",
-        "symbol_orientation",
-        [
-          "case",
-          ["has", "dip", ["get", "orientation"]],
-          ["get", "dip", ["get", "orientation"]],
-          [
-            "case",
-            ["has", "plunge", ["get", "orientation"]],
-            ["get", "plunge", ["get", "orientation"]],
-            0,
-          ],
-        ],
-        [
-          "let",
-          "feature_type",
-          ["get", "feature_type", ["get", "orientation"]],
-
-          // Output
-          [
-            "case",
-            // Case 1: Orientation has facing
-            [
-              "all",
-              ["==", ["get", "facing", ["get", "orientation"]], "overturned"],
-              ["any", ["==", ["var", "feature_type"], "bedding"]],
-            ],
-            [
-              "concat",
-              ["get", "feature_type", ["get", "orientation"]],
-              "_overturned",
-            ],
-            [
-              "case",
-              // Case 2: Symbol orientation is 0 and feature type is bedding or foliation
-              [
-                "all",
-                ["==", ["var", "symbol_orientation"], 0],
-                [
-                  "any",
-                  ["==", ["var", "feature_type"], "bedding"],
-                  ["==", ["var", "feature_type"], "foliation"],
-                ],
-              ],
-              ["concat", ["var", "feature_type"], "_horizontal"],
-              [
-                "case",
-                // Case 3: Symbol orientation between 0-90 and feature type is bedding, contact, foliation or shear zone
-                [
-                  "all",
-                  [">", ["var", "symbol_orientation"], 0],
-                  ["<", ["var", "symbol_orientation"], 90],
-                  [
-                    "any",
-                    ["==", ["var", "feature_type"], "bedding"],
-                    ["==", ["var", "feature_type"], "contact"],
-                    ["==", ["var", "feature_type"], "foliation"],
-                    ["==", ["var", "feature_type"], "shear_zone"],
-                  ],
-                ],
-                ["concat", ["var", "feature_type"], "_inclined"],
-                [
-                  "case",
-                  // Case 4: Symbol orientation is 90 and feature type is bedding, contact, foliation or shear zone
-                  [
-                    "all",
-                    ["==", ["var", "symbol_orientation"], 90],
-                    [
-                      "any",
-                      ["==", ["var", "feature_type"], "bedding"],
-                      ["==", ["var", "feature_type"], "contact"],
-                      ["==", ["var", "feature_type"], "foliation"],
-                      ["==", ["var", "feature_type"], "shear_zone"],
-                    ],
-                  ],
-                  ["concat", ["var", "feature_type"], "_vertical"],
-                  [
-                    "case",
-                    // Case 5: Other features with no symbol orienation
-                    [
-                      "all",
-                      ["has", "feature_type", ["get", "orientation"]],
-                      [
-                        "any",
-                        ["==", ["var", "feature_type"], "fault"],
-                        ["==", ["var", "feature_type"], "fracture"],
-                        ["==", ["var", "feature_type"], "vein"],
-                      ],
-                    ],
-                    ["get", "feature_type", ["get", "orientation"]],
-                    [
-                      "case",
-                      // Defaults
-                      [
-                        "==",
-                        ["get", "type", ["get", "orientation"]],
-                        "linear_orientation",
-                      ],
-                      "lineation_general",
-                      "default_point",
-                    ],
-                  ],
-                ],
-              ],
-            ],
-          ],
-        ],
-      ],
-      "default_point",
-    ];
-  };
-
-  function getIconImageExt() {
-    /** Extension to Strabo-provided getIconImage that modifies the style tree to use programmatic definition of icon image if provided. */
-    return [
-      "case",
-      ["has", "symbol_name"],
-      ["get", "symbol_name"],
-      getIconImage(),
-    ];
-  }
 
   const symbolLayers = [
     {
