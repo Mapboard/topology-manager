7ca170fc86ca0901489e287826f71db3be4236ee
Daven Quinn <code@davenquinn.com>
2024-03-27 10:42:33 -0500

    Updated schema design

--- mapboard/topology_manager/tests/fixtures/demo_units/procedures/03-add-to-map.sql
+++ mapboard/topology_manager/tests/fixtures/demo_units/procedures/03-add-to-map.sql
@@ -1,21 +1,25 @@
-INSERT INTO {topo_schema}.subtopology (id)
+INSERT INTO {data_schema}.map_layer (id, name)
 VALUES
-  ('surficial'),
-  ('bedrock')
-ON CONFLICT DO NOTHING;
+  ('surficial', 'Surficial'),
+  ('bedrock', 'Bedrock');
 
 DELETE FROM {data_schema}.linework_type WHERE id = 'default';
 DELETE FROM {data_schema}.polygon_type WHERE id = 'default';
 
 INSERT INTO {data_schema}.linework_type
-SELECT id, name, color, topology
+SELECT id, name, color
 FROM tmp_linework_type
 ON CONFLICT (id)
 DO UPDATE
 SET
 name = EXCLUDED.name,
-color = EXCLUDED.color,
-topology = EXCLUDED.topology;
+color = EXCLUDED.color;
+
+INSERT INTO {data_schema}.map_layer_linework_type (layer, type)
+SELECT DISTINCT ON (layer, id)
+  layer, id
+FROM tmp_linework_type
+ON CONFLICT DO NOTHING;
 
 DELETE FROM {data_schema}.linework_type
 WHERE id NOT IN (SELECT id FROM tmp_linework_type);
@@ -31,8 +35,13 @@ ON CONFLICT (id)
 DO UPDATE
 SET
 name = EXCLUDED.name,
-color = EXCLUDED.color,
-topology = EXCLUDED.topology;
+color = EXCLUDED.color;
+
+INSERT INTO {data_schema}.map_layer_polygon_type (layer, type)
+SELECT DISTINCT ON (layer, id)
+  layer, id
+FROM tmp_polygon_type
+ON CONFLICT DO NOTHING;
 
 DELETE FROM {data_schema}.polygon_type
 WHERE id NOT IN (SELECT id FROM tmp_polygon_type);
