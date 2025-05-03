1a6fac31894a8f09606ae25802ed82bd8713bd7a
Daven Quinn <code@davenquinn.com>
2025-05-03 02:18:25 -0500

    Some things seem to not be atomic

--- mapboard/topology_manager/tests/test_02_multi_layers.py
+++ mapboard/topology_manager/tests/test_02_multi_layers.py
@@ -15,6 +15,8 @@ from .helpers import (
 from ..database import sql
 from ..update_faces import get_adjacent_faces
 
+from pytest import fixture
+
 
 def test_topo_face_no_identifier(db):
     """Test that a face with no identifier is created"""
@@ -49,32 +51,41 @@ def test_new_layer(db):
     assert n_faces(db) == 1
 
 
+@fixture(scope="class")
+def layers(db):
+    bedrock_id = map_layer_id(db, "bedrock")
+    surficial_id = map_layer_id(db, "surficial")
+
+    # Insert a square
+    insert_line(db, square(6, center=(3, 3)), type="bedrock", map_layer=bedrock_id)
+
+    # Insert a smaller square with the surficial type
+    insert_line(
+        db, square(2, center=(3, 3)), type="surficial", map_layer=surficial_id
+    )
+
+    # Add identifying units
+    insert_polygon(
+        db, square(1, center=(3, 3)), type="upper-omkyk", map_layer=bedrock_id
+    )
+
+    insert_polygon(
+        db, square(1, center=(3, 3)), type="terrace", map_layer=surficial_id
+    )
+
+    # Solve the topology
+    _update(db)
+
+    return {
+        "bedrock": bedrock_id,
+        "surficial": surficial_id,
+    }
+
+
 class TestMultiLayers:
-    def test_multi_layers(self, db):
+    def test_multi_layers(self, db, layers):
         """Insert two overlapping squares that belong to different sub-topologies"""
         # Check if map layer is integer
-        bedrock_id = map_layer_id(db, "bedrock")
-        surficial_id = map_layer_id(db, "surficial")
-
-        # Insert a square
-        insert_line(db, square(6, center=(3, 3)), type="bedrock", map_layer=bedrock_id)
-
-        # Insert a smaller square with the surficial type
-        insert_line(
-            db, square(2, center=(3, 3)), type="surficial", map_layer=surficial_id
-        )
-
-        # Add identifying units
-        insert_polygon(
-            db, square(1, center=(3, 3)), type="upper-omkyk", map_layer=bedrock_id
-        )
-
-        insert_polygon(
-            db, square(1, center=(3, 3)), type="terrace", map_layer=surficial_id
-        )
-
-        # Solve the topology
-        _update(db)
 
         # Check that we have two map faces at the center
         res = intersecting_faces(
@@ -85,39 +96,39 @@ class TestMultiLayers:
         has_bedrock = False
         has_surficial = False
         for r in res:
-            if r.map_layer == bedrock_id:
+            if r.map_layer == layers["bedrock"]:
                 has_bedrock = True
                 assert r.area == 36.0
-            if r.map_layer == surficial_id:
+            if r.map_layer == layers["surficial"]:
                 has_surficial = True
                 assert r.area == 4.0
         assert has_bedrock
         assert has_surficial
 
     @mark.skip("This does not work with savepoints")
-    def test_remove_bedrock(self, db):
-        bedrock_id = map_layer_id(db, "bedrock")
+    def test_remove_bedrock(self, db, layers):
         assert n_faces(db) == 2
-        assert n_faces(db, map_layer=bedrock_id) == 1
+        assert n_faces(db, map_layer=layers["bedrock"]) == 1
 
         # This works with savepoints but not nested transactions
         with db.savepoint(rollback="always"):
-            _test_internals(db)
+            _test_internals(db, layers)
 
-    def test_remove_bedrock_no_nested_transaction(self, db):
-        _test_internals(db)
+    def test_remove_bedrock_no_nested_transaction(self, db, layers):
+        _test_internals(db, layers)
 
-    def test_remove_surficial(self, db):
+    def test_remove_surficial(self, db, layers):
         assert n_faces(db) == 1
-        assert n_faces(db, map_layer=map_layer_id(db, "surficial")) == 1
+        assert n_faces(db, map_layer=layers["surficial"]) == 1
         with db.savepoint(rollback="always"):
-            db.run_query("DELETE FROM {data_schema}.linework WHERE type = 'surficial'")
+            db.run_query("DELETE FROM {data_schema}.linework WHERE map_layer = :map_layer",
+                         {"map_layer": layers["surficial"]})
             _update(db)
             assert n_faces(db) == 0
 
 
-def _test_internals(db):
-    bedrock_id = map_layer_id(db, "bedrock")
+def _test_internals(db, layers):
+    bedrock_id = layers["bedrock"]
     assert n_faces(db) == 2
     db.run_sql("DELETE FROM {data_schema}.linework WHERE map_layer = :map_layer", {"map_layer": bedrock_id})
 
