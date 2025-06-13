011ee8d890ceb20b2d6417e25f782d809e565430
Daven Quinn <code@davenquinn.com>
2025-06-13 00:56:47 -0500

    Added in-progress tests of composite layers

---
+++ mapboard/topology_manager/tests/test_06_composite_layer.py
@@ -0,0 +1,126 @@
+"""Tests to ensure efficient calculations of map faces."""
+
+from macrostrat.utils.timer import Timer
+from macrostrat.utils import get_logger
+
+from .helpers import (
+    insert_line,
+    map_layer_id,
+    add_linework_type_to_layer,
+    n_faces,
+    n_face_primitives,
+    create_map_layer,
+)
+from ..commands.update import _update
+from pytest import mark
+
+log = get_logger(__name__)
+
+
+class TestCompositeLayers:
+    def test_create_nested_map_layers(self, db):
+        """Create a parent and child map layer."""
+
+        grandparent_lyr = create_map_layer(db, "map-area")
+        parent_lyr = create_map_layer(db, "tectonic-block", parent=grandparent_lyr)
+        child_lyr = create_map_layer(db, "bedrock", parent=parent_lyr)
+
+        # Surficial layer
+        create_map_layer(db, "surficial", parent=grandparent_lyr)
+
+        # Create linework types
+        add_linework_type_to_layer(db, child_lyr, "contact")
+
+        # add a linework type
+        add_linework_type_to_layer(db, child_lyr, "bedrock")
+
+    @mark.parametrize("count_on_each_axis", [1, 5, 10])
+    def test_create_faces_with_overlapping_lines(self, db, count_on_each_axis):
+        """Create overlapping sets of lines to test face creation."""
+        child_lyr = map_layer_id(db, "child")
+        parent_lyr = map_layer_id(db, "parent")
+
+        timer = Timer()
+        with timer.context():
+            for x in range(count_on_each_axis + 1):
+                insert_line(
+                    db,
+                    ((x, 0), (x, count_on_each_axis)),
+                    type="bedrock",
+                    map_layer=child_lyr,
+                )
+            for y in range(count_on_each_axis + 1):
+                insert_line(
+                    db,
+                    ((0, y), (count_on_each_axis, y)),
+                    type="bedrock",
+                    map_layer=child_lyr,
+                )
+
+            Timer.add_step("insert-lines")
+
+            # Solve the faces
+            _update(db)
+
+            Timer.add_step("update")
+
+            # Check that we have 100 map faces
+            assert n_faces(db) == count_on_each_axis**2
+            assert n_face_primitives(db) == count_on_each_axis**2
+
+        log.info(timer.server_timings())
+
+    def test_add_parent_layer(self, db):
+        # Add a parent layer with encompassing faces
+        parent_lyr = map_layer_id(db, "parent")
+        add_linework_type_to_layer(db, parent_lyr, "bedrock")
+
+        # Insert a line outside of the child layer that encompasses all child faces
+        coords = [
+            (-1, -1),
+            (11, -1),
+            (11, 11),
+            (-1, 11),
+            (-1, -1),
+        ]
+        insert_line(db, coords, type="bedrock", map_layer=parent_lyr)
+
+        # Solve the topology
+        _update(db)
+
+        # Check that we have 102 map faces and one fewer primitive.
+        # - The child layer now has 101 faces including the ring outside the 10x10 grid
+        # - The parent layer has 1 face that is 12x12 units and encompasses all child faces
+        # - Primitives are shared between layers so there are 101
+        assert n_face_primitives(db) == 101
+        assert n_faces(db) == 102
+
+    def test_erase_and_consolidate_faces(self, db):
+        """Test the erasure and consolidation of faces."""
+        _child_layer = map_layer_id(db, "child")
+        _parent_lyr = map_layer_id(db, "parent")
+
+        # Delete lines that cover the bottom right corner of the child layer
+        # while leaving other lines intact. This should remove 4 faces from the
+        db.run_sql(
+            """
+            UPDATE {data_schema}.linework
+            SET geometry = ST_Difference(geometry, ST_MakeEnvelope(0.1, 0.1, 1.98, 1.98, :srid))
+            WHERE TYPE = 'bedrock'
+              AND map_layer = :child_lyr
+            """,
+            dict(child_lyr=_child_layer),
+        )
+
+        _update(db)
+
+        # We should have merged 4 faces into 1 in the child layer
+        # - 101-4+1=98 faces in child layer
+        # - one face in parent layer
+        # - 99 total faces
+
+        assert n_faces(db, map_layer=_child_layer) == 98
+        assert n_faces(db, map_layer=_parent_lyr) == 1
+
+        assert n_faces(db) == 99
+        assert n_face_primitives(db) == 98
