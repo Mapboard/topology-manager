0c2d1b6748e17007af939e05b72d38fd919b13f0
Daven Quinn <code@davenquinn.com>
2025-06-17 12:09:18 -0500

    Generalized a test

--- mapboard/topology_manager/tests/test_06_composite_layer.py
+++ mapboard/topology_manager/tests/test_06_composite_layer.py
@@ -9,8 +9,10 @@ from pytest import mark
 
 log = get_logger(__name__)
 
-parent_layer_name = "tectonic-block"
+parent_layer_name = ("tectonic-block")
 child_layer_name = "rocks"
+
+grid_count_on_each_axis = 7
 
 class TestCompositeLayers:
     def test_create_nested_map_layers(self, db):
@@ -24,24 +26,24 @@ class TestCompositeLayers:
         # add a linework type
         add_linework_type_to_layer(db, child_lyr, "bedrock")
 
-    @mark.parametrize("count_on_each_axis", [1, 5, 10])
-    def test_create_faces_with_overlapping_lines(self, db, count_on_each_axis):
+    def test_create_bedrock_grid(self, db):
         """Create overlapping sets of lines to test face creation."""
+        count_on_each_axis = 5
         child_lyr = map_layer_id(db, child_layer_name)
 
         timer = Timer()
         with timer.context():
-            for x in range(count_on_each_axis + 1):
+            for x in range(grid_count_on_each_axis + 1):
                 insert_line(
                     db,
-                    ((x, 0), (x, count_on_each_axis)),
+                    ((x, 0), (x, grid_count_on_each_axis)),
                     type="bedrock",
                     map_layer=child_lyr,
                 )
-            for y in range(count_on_each_axis + 1):
+            for y in range(grid_count_on_each_axis + 1):
                 insert_line(
                     db,
-                    ((0, y), (count_on_each_axis, y)),
+                    ((0, y), (grid_count_on_each_axis, y)),
                     type="bedrock",
                     map_layer=child_lyr,
                 )
@@ -54,8 +56,8 @@ class TestCompositeLayers:
             Timer.add_step("update")
 
             # Check that we have 100 map faces
-            assert n_faces(db) == count_on_each_axis ** 2
-            assert n_face_primitives(db) == count_on_each_axis ** 2
+            assert n_faces(db) == grid_count_on_each_axis ** 2
+            assert n_face_primitives(db) == grid_count_on_each_axis ** 2
 
         log.info(timer.server_timings())
 
@@ -65,11 +67,13 @@ class TestCompositeLayers:
         add_linework_type_to_layer(db, parent_lyr, "bedrock")
 
         # Insert a line outside of the child layer that encompasses all child faces
+        _max = grid_count_on_each_axis + 1
+
         coords = [
             (-1, -1),
-            (11, -1),
-            (11, 11),
-            (-1, 11),
+            (_max, -1),
+            (_max, _max),
+            (-1, _max),
             (-1, -1),
         ]
         insert_line(db, coords, type="bedrock", map_layer=parent_lyr)
@@ -81,8 +85,9 @@ class TestCompositeLayers:
         # - The child layer now has 101 faces including the ring outside the 10x10 grid
         # - The parent layer has 1 face that is 12x12 units and encompasses all child faces
         # - Primitives are shared between layers so there are 101
-        assert n_face_primitives(db) == 101
-        assert n_faces(db) == 102
+
+        assert n_face_primitives(db) == grid_count_on_each_axis**2 + 1
+        assert n_faces(db) == grid_count_on_each_axis**2 + 2
 
     def test_erase_and_consolidate_faces(self, db):
         """Test the erasure and consolidation of faces."""
@@ -103,13 +108,8 @@ class TestCompositeLayers:
 
         _update(db)
 
-        # We should have merged 4 faces into 1 in the child layer
-        # - 101-4+1=98 faces in child layer
-        # - one face in parent layer
-        # - 99 total faces
-
-        assert n_faces(db, map_layer=_child_layer) == 98
+        assert n_faces(db, map_layer=_child_layer) == grid_count_on_each_axis**2 + 1 - 4 + 1
         assert n_faces(db, map_layer=_parent_lyr) == 1
 
-        assert n_faces(db) == 99
-        assert n_face_primitives(db) == 98
+        assert n_faces(db) == grid_count_on_each_axis**2 + 2 - 4 + 1
+        assert n_face_primitives(db) == grid_count_on_each_axis**2 - 3 + 1
