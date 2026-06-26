c81e6c2610a0e87d877ba6d021522a544f74deb5
Daven Quinn <code@davenquinn.com>
2026-06-26 02:15:06 -0500

    Add slightly more control over the topology updating functions

--- mapboard/topology_manager/commands/update_topology.py
+++ mapboard/topology_manager/commands/update_topology.py
@@ -18,45 +18,55 @@ def update(
     fix_failed: bool = False,
     incremental: bool = False,
     composite_layers: bool = False,
+    boundaries: bool = True,
+    faces: bool = True,
 ):
     """Update the topology"""
     if ctx is None:
         ctx = get_context()
 
-    t_start = perf_counter()
+    n_contacts_updated = 0
 
-    console.print("Updating boundaries", style="header")
-    n_contacts_updated = update_contacts(ctx, fix_failed=fix_failed)
-    t1 = perf_counter()
-    print_step("Update boundaries", t1 - t_start)
+    t_init = perf_counter()
+    t_step = t_init
+
+    if boundaries:
+        console.print("Updating boundaries", style="header")
+        n_contacts_updated = update_contacts(ctx, fix_failed=fix_failed)
+        t1 = perf_counter()
+        print_step("Update boundaries", t1 - t_step)
+        t_step = t1
 
     if n_contacts_updated > 0:
         console.print("Cleaning topology (pre-faces)", style="header")
         clean_topology(ctx)
         t2 = perf_counter()
-        print_step("Clean topology (pre-faces)", t2 - t1)
-    else:
-        t2 = t1
+        print_step("Clean topology (pre-faces)", t2 - t_step)
+        t_step = t2
 
-    console.print("Updating faces", style="header")
-    update_faces(
-        ctx,
-        reset=reset,
-        fill_holes=fill_holes,
-        incremental=incremental,
-    )
-    t3 = perf_counter()
-    print_step("Update faces", t3 - t2)
+    if faces:
+        console.print("Updating faces", style="header")
+        update_faces(
+            ctx,
+            reset=reset,
+            fill_holes=fill_holes,
+            incremental=incremental,
+        )
+        t3 = perf_counter()
+        print_step("Update faces", t3 - t_step)
+        t_step = t3
 
-    console.print("Cleaning topology", style="header")
-    clean_topology(ctx)
-    t4 = perf_counter()
-    print_step("Clean topology", t4 - t3)
+    if boundaries or faces:
+        console.print("Cleaning topology", style="header")
+        clean_topology(ctx)
+        t4 = perf_counter()
+        print_step("Clean topology", t4 - t_step)
+        t_step = t4
 
     if composite_layers:
         console.print("Updating composite layers", style="header")
         update_composite_layers(ctx)
         t5 = perf_counter()
-        print_step("Update composite layers", t5 - t4)
+        print_step("Update composite layers", t5 - t_step)
 
-    print_step("Total", perf_counter() - t_start)
+    print_step("Total", perf_counter() - t_step)
