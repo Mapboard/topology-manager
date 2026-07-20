12416e1e06ed10b54999b7c1cc42088a8a049e76
Daven Quinn <code@davenquinn.com>
2026-07-20 05:41:49 -0500

    Improved speed of topology cleaning functions

--- mapboard/topology_manager/procedures/clean-topology/remove-empty-topogeometries.sql
+++ mapboard/topology_manager/procedures/clean-topology/remove-empty-topogeometries.sql
@@ -6,17 +6,20 @@
   The clearTopoGeom procedure can be used instead of this one to
   remove old elements when a topogeometry is modified.
  */
-WITH deleted AS (
-  DELETE FROM {topo_schema}.relation r
-  WHERE r.layer_id = :layer_id
-  AND NOT EXISTS (
-    SELECT 1
-    FROM {table} a
-    WHERE ({feature_column}).layer_id = :layer_id
-      AND ({feature_column}).id = r.topogeo_id
-      AND ({feature_column}).type = :feature_type
-      AND ({feature_column}).topology_id = :topology_id
+WITH stale_ids AS (
+  SELECT topogeo_id FROM {topo_schema}.relation
+  WHERE layer_id = :layer_id AND element_type = :feature_type
+  EXCEPT
+  SELECT ({feature_column}).id FROM {table}
+  WHERE topo IS NOT NULL
+    AND ({feature_column}).layer_id = :layer_id
+    AND ({feature_column}).type = :feature_type
+    AND ({feature_column}).topology_id = :topology_id
+),
+  deleted AS (
+    DELETE FROM {topo_schema}.relation r
+      WHERE r.layer_id = :layer_id
+        AND r.topogeo_id IN (SELECT topogeo_id FROM stale_ids)
+      RETURNING r.topogeo_id
   )
-  RETURNING r.topogeo_id
-)
 SELECT count(*) FROM deleted;
