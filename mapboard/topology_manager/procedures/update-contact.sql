3b28a6eef5b87cf8d2d0926c3ef74c1320741241
Daven Quinn <code@davenquinn.com>
2026-07-20 02:19:49 -0500

    tunable contact insertions speed

--- mapboard/topology_manager/procedures/update-contact.sql
+++ mapboard/topology_manager/procedures/update-contact.sql
@@ -5,5 +5,5 @@ FROM {boundary_table} l
 WHERE {topo_schema}.get_topological_map_layer(l) IS NOT null
   AND l.topology_error IS NULL
   AND geometry_hash IS NULL
-ORDER BY ST_GeoHash(geometry) DESC
+ORDER BY ST_MemSize(geometry) DESC
 LIMIT :n
